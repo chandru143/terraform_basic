64b40867274819d4c3f6cd32f943da66e74c75d7
chandru143 <chandarchandar143@yahoo.co.in>
2022-12-30 13:53:43 +0000

    updated tf file

---
+++ variables/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+variable "vpc_name" {
+  type = string
+  default = "my_vpc"
+}
+
+variable "sshport" {
+  type = number
+  default = 22
+}
+
+variable "enabled" {
+    default = true
+}
+
+variable "mylist" {
+  type = list(string)
+  default = [ "value1","value2" ]
+}
+
+variable "mympap" {
+  type = map
+  default = {
+    key1 = "vaule1"
+    key2 = "vaule2"
+  }
+}
+
+variable "myinput" {
+    type = string
+    description = "Enter the vpc name"
+}
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      "Name" = var.myinput
+    }
+}
+output "arn" {
+  value = aws_vpc.myvpc.arn
+}
